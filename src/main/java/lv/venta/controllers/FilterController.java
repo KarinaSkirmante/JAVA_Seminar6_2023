e22e15d25737844454e0c4159a4c1a8dada50fc3
localuser <localuser@VEA.LV>
2023-05-25 16:01:05 +0300

    Add: added controller for courses by student

--- src/main/java/lv/venta/controllers/FilterController.java
+++ src/main/java/lv/venta/controllers/FilterController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.services.IFilteringService;
 
@@ -57,7 +58,21 @@ public class FilterController {
 		}
 	
 	
-	
+	//TODO filter all courses by student by id
+	//TODO please use ?
+	@GetMapping("/info/showAllCourses/students")
+	public String getShowAllCoursesByStudentId(@RequestParam(name="id") long id,
+			Model model) {
+		try
+		{
+			model.addAttribute("courses", filterService.retrieveAllCoursesByStudentId(id));
+			return "all-courses-page";
+		}
+		catch (Exception e) {
+			model.addAttribute("msg", e.getMessage());
+			return "error-page";//will show error-page.html
+		}
+	}
 	
 	
 	
